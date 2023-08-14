838d574b147f5ae8028c6e10a68384082b9c417f
Julien Crutain <juju510@hotmail.fr>
2023-08-14 12:21:27 +0200

    add navbar

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    colors:{
+      transparent: 'transparent',
+      current: 'currentColor',
+      'brun_terreux': '#7B6C3E',
+      'beige_chaud': '#D4C5A5',
+      'gris_ardoise': '#8F8F8F',
+      'brun_terreux_clair': '#CFC4A0',
+      'beige_chaud_clair': '#EFE9DC',
+      'gris_ardoise_clair': '#E6E6E6',
+    },
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
