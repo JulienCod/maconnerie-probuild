452e0678fcf33bbae983304ef9095b1a59848bdf
Julien Crutain <juju510@hotmail.fr>
2023-08-17 12:33:58 +0200

    add admin contact view and delete

---
+++ src/Controller/Admin/AdminContactController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/contact', name: 'admin_contact_')]
+class AdminContactController extends AbstractController
+{
+    private $entityManager;
+    private $contactRepository;
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        ContactRepository $contactRepository
+    )
+    {
+        $this->entityManager = $entityManager;
+        $this->contactRepository = $contactRepository;
+    }
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        $dataContacts = $this->entityManager->getRepository(Contact::class)->findAll();
+        $contacts = [];
+
+        foreach ($dataContacts as $contact){
+            $contact = [
+                "id" => $contact->getId(),
+                "fullName" => $contact->getFullName(),
+                "email" => $contact->getEmail(),
+                "subject" => $contact->getSubject(),
+                "content" => $contact->getContent(),
+                "phoneNumber" => $contact->getPhoneNumber(),
+                "createdAt" => $contact->getCreatedAt()->format('Y-m-d H:i:s'),
+                "isView" => $contact->isIsView(),
+            ];
+            $contacts[] = $contact;
+        }
+        return $this->render('admin/contact/index.html.twig',compact('contacts'));
+    }
+
+    #[Route('/{id}', name: 'view')]
+    public function view($id): Response
+    {
+        $data = $this->contactRepository->find($id);
+        
+        $contact = [
+                "id" => $data->getId(),
+                "fullName" => $data->getFullName(),
+                "email" => $data->getEmail(),
+                "subject" => $data->getSubject(),
+                "content" => $data->getContent(),
+                "phoneNumber" => $data->getPhoneNumber(),
+                "createdAt" => $data->getCreatedAt()->format('Y-m-d H:i:s'),
+                "isView" => $data->isIsView(),
+            ];
+
+        return $this->render('admin/contact/view.html.twig',compact('contact'));
+    
+    }
+    #[Route('/{id}/supprimer', name: 'delete')]
+    public function delete($id): Response
+    {
+        $contact = $this->contactRepository->find($id);
+        
+        if (!$contact){
+            $this->addFlash('danger', 'le message n\'existe pas');
+        }
+        
+        $this->entityManager->remove($contact);
+        $this->entityManager->flush();
+        
+        
+        $this->addFlash('success', 'le message a bien été supprimé');
+
+        return $this->redirectToRoute('admin_contact_index');
+    }
+}
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/admin/dashboard', name: 'admin_dashboard')]
+    public function index(
+        EntityManagerInterface $entityManager,
+    ): Response
+    {
+        // mettre en place un récapitulatif des données
+        $countContact = count($entityManager->getRepository(Contact::class)->findAll());
+        return $this->render('admin/dashboard/index.html.twig',compact('countContact'));
+    }
+}
